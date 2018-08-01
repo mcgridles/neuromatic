cc13abb9ed03a6254135bca05c3325e14ac45166
Griffin Knipe <34732398+knipegp@users.noreply.github.com>
2018-08-01 14:15:14 -0400

    status box resizing (#4)

--- interface/status_box.py
+++ interface/status_box.py
@@ -3,23 +3,27 @@ import tkinter as tk
 
 class StatusBox(object):
 
-    def __init__(self, root, height=200, width=200):
-        self.frame = tk.Frame(root, height=height, width=width, pady=3, padx=3)
+    def __init__(self, root):
+        self.frame = tk.Frame(root, pady=3, padx=3)
         self.frame.columnconfigure(0, weight=1)
-        self.frame.rowconfigure(0, weight=1)
-        self.frame.grid(sticky='nsew')
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.grid(row=0, column=0, sticky='nsew')
+        self.frame.grid_propagate(0)
+
+        self.label = tk.Label(self.frame, text='Status', font='Helvetica 12 bold')
+        self.label.grid(row=0, column=0, sticky='w')
 
         self.text = tk.Text(self.frame)
         self.text.config(state=tk.DISABLED, wrap=tk.NONE)
-        self.text.grid(row=0, column=0, stick='nsew')
+        self.text.grid(row=1, column=0, sticky='nsew')
 
         self.v_scroll = tk.Scrollbar(self.frame, orient=tk.VERTICAL)
-        self.v_scroll.grid(row=0, column=1, stick='ns')
+        self.v_scroll.grid(row=1, column=1, sticky='ns')
         self.v_scroll.config(command=self.text.yview)
         self.text.config(yscrollcommand=self.v_scroll.set)
 
         self.h_scroll = tk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
-        self.h_scroll.grid(row=1, column=0, stick='ew')
+        self.h_scroll.grid(row=2, column=0, sticky='ew')
         self.h_scroll.config(command=self.text.xview)
         self.text.config(xscrollcommand=self.h_scroll.set)
 
@@ -52,18 +56,34 @@ class StatusBox(object):
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('StatusBox Test')
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_rowconfigure(0, weight=1)
+    root.geometry('400x400')
+
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=1)
 
     text_frame = tk.Frame(root, bg='green', height=200, width=200, pady=3, padx=3)
-    text_frame.grid(row=0, column=0, sticky='nsew')
+    text_frame.grid(row=1, column=1, sticky='nsew')
     text_frame.rowconfigure(0, weight=1)
     text_frame.columnconfigure(0, weight=1)
-    box = StatusBox(text_frame)
 
     button_frame = tk.Frame(root, bg='yellow', height=200, width=200, pady=3, padx=3)
     button_frame.grid(row=0, column=1, sticky='nsew')
+    button_frame.rowconfigure(0, weight=1)
+    button_frame.columnconfigure(0, weight=1)
+
+    blank_frame_1 = tk.Frame(root, bg='blue', height=200, width=200, pady=3, padx=3)
+    blank_frame_1.grid(row=0, column=0, sticky='nsew')
+
+    blank_frame_2 = tk.Frame(root, bg='cyan', height=200, width=200, pady=3, padx=3)
+    blank_frame_2.grid(row=1, column=0, sticky='nsew')
+
+    box = StatusBox(text_frame)
+
     button = tk.Button(button_frame, command=lambda: box.add_text('test\n'), text="add text")
-    button.pack()
+    button.grid(row=0, column=0)
+    # button.pack()
 
     root.mainloop()
